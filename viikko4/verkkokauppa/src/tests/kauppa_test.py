527170142cf67bf2ec4f91d0b41e31b1b13c95d4
aarokoivula <aaro.koivula@gmail.com>
2025-11-21 17:23:45 +0200

    git tag change 1

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -104,7 +104,7 @@ class TestKauppa(unittest.TestCase):
 
         self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
     
-    def test_korista_poistamisen_jälkeen_oikea_summa_on_veloitettu(self):
+    def test_korista_poistamisen_jälkeen_oikea_summa_veloitetaan(self):
         self.kauppa.aloita_asiointi()
         self.kauppa.lisaa_koriin(1)
         self.kauppa.lisaa_koriin(2)
